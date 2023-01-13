4fa16be0e8ab510a6bb4296457ca7a4c0834c3b8
alessandrolega <alessandrolega24@gmail.com>
2023-01-13 01:25:20 +0100

    create pagine + partials + routes + header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+    <!--FAKE HEADER-->
+    <div class="fake-header">
+        <div class="fake-header-cont">
+            <ul class="ul-fake-header">
+                <li class="fake-header-li">DC POWER VISA <span>&#174;</span></li>
+                <li class="fake-header-li">ADDITIONAL DC SITES <span>&#8711;</span></li>
+            </ul>
+
+        </div>
+
+    </div>
+
+    <header>
+        <div class="header-cont">
+             <!--LOGO CONT-->
+        <div class="logo-cont">
+            <img src="{{asset('img/dc-logo.png')}}" alt="logo">
+        </div>
+        <!--NAVBAR MENU-->
+        <nav>
+            <ul class="header-ul">
+                <li class="header-li"><a class="navA" href="#">CHARACTERS</a></li>
+                <li class="header-li"><a class="navA" href="#">COMICS</a></li>
+                <li class="header-li"><a class="navA" href="#">MOVIES</a></li>
+                <li class="header-li"><a class="navA" href="#">TV</a></li>
+                <li class="header-li"><a class="navA" href="#">GAMES</a></li>
+                <li class="header-li"><a class="navA" href="#">COLLECTIBLES</a></li>
+                <li class="header-li"><a class="navA" href="#">VIDEOS</a></li>
+                <li class="header-li"><a class="navA" href="#">FANS</a></li>
+                <li class="header-li"><a class="navA" href="#">NEWS</a></li>
+                <li class="header-li"><a class="navA" href="#">SHOP</a></li>
+            </ul>
+        </nav>
+        <!--SEARCH ICON-->
+        <div class="search-cont">
+            <span class="span-search">Search <i class="fa-solid fa-magnifying-glass"></i></span>
+            <div class="line-blue"></div>
+        </div>
+        </div>  
+    </header>
+</body>
+</html>
